61ba5a352f2ff2ccff672c4e4c0ccc9c262ecc34
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2024-05-06 21:44:26 +0700

    fix: indexing in comments

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def get_data() -> str:
 def add_data(name: str) -> None:
     data.append(name)
 
-def rename_data(index: int, newName: str) -> None:
-    data[index] = newName
+def rename_data(index: int, newname: str) -> None:
+    data[index] = newname
 
 def remove_data(index: int) -> None:
     del data[index]
@@ -24,30 +24,35 @@ def roll_random() -> None:
 
 
 if __name__ == '__main__':
-    while True:
+    while True: #1
         main_menu = questionary.select(
             f"Welcome to Random Picker!\n{get_data()}",
             choices=["Make new partisipants", "Edit partisipants", "ROLL RANDOM!", "Quit"],
-        ).ask()
-        if main_menu == 'Make new partisipants':
-            add_data(questionary.text("Full name").ask())
+        ).ask() #2
+        if main_menu == 'Make new partisipants': #3
+            add_data(questionary.text("Full name").ask()) #4
         elif main_menu == 'Edit partisipants':
             choose_index = data.index(
                     questionary.select(
-                    f"Choose the participant!",
+                    "Choose the participant!",
                     choices=data,
                 ).ask()
-            )
+            ) #5
             edit_menu = questionary.select(
                 f"Choose the participant!\n{data[choose_index]}",
                 choices=["Edit name", "Remove", "Back"],
-            ).ask()
-            if edit_menu == 'Edit name':
-                rename_data(choose_index, questionary.text("Full name").ask())
+            ).ask() #6
+            if edit_menu == 'Edit name': #7
+                rename_data(choose_index, questionary.text("Full name").ask()) #8
             elif edit_menu == 'Remove':
-                remove_data(choose_index)
+                remove_data(choose_index) #9
+            #10
         elif main_menu == 'ROLL RANDOM!':
-            if len(data) > 1:
-                roll_random()
+            if len(data) > 1: #11
+                roll_random() #12
+            else:
+                pass #13
         elif main_menu == 'Quit':
-            break
+            break #14
+        #15
+    quit() #16
